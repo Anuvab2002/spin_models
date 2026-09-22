335aa01e06afffcb9478667c0eacc33c34de8d20
Anuvab2002 <anuvabpanda2002@gmail.com>
2026-09-22 14:13:30 +0530

    test controller module

---
+++ src/test_controller.f90
@@ -0,0 +1,8 @@
+module test_controller_m
+contains
+  subroutine test_driver()
+    implicit none
+    !
+    write(*,*) "Hi Po! Reporting from test controller. :-)>"
+  end subroutine test_driver
+end module test_controller_m
