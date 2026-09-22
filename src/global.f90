888f5ce31ca3fba4c9b9bec4e43cde039f302fb7
Anuvab2002 <anuvabpanda2002@gmail.com>
2026-09-22 14:11:53 +0530

    subroutine for global parameters

---
+++ src/global.f90
@@ -0,0 +1,9 @@
+!> @file global.f90
+!> @brief provides gloval parameters
+!> @author ap
+module global_m
+  double precision, parameter           :: zero=1.0d-8! zero
+  double precision, parameter           :: tol=1.0d-5! tolerance
+  complex(8), parameter                 :: iota=(0.d0,1.d0)! iota
+  double precision, parameter           :: pi=4.d0*atan(1.d0)! pi
+end module global_m
